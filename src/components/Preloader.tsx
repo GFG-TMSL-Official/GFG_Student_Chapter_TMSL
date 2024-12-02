c303c55bdbebdd3f5d18d49a529fc9df9fbce443
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-12-02 21:18:52 +0530

    Update Preloader.tsx

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import './Preloader.css'; // Import the CSS file for styling
+
+const Preloader: React.FC = () => {
+    return (
+        <div className="preloader">
+            <div className="spinner"></div>
+        </div>
+    );
+};
+
+export default Preloader;
 
