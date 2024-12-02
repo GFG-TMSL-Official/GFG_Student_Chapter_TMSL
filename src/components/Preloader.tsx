4c8e897de33929ff84b66f54cab8bd15fb92d658
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-12-02 23:02:46 +0530

    Update Preloader.tsx

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -3,32 +3,38 @@ import './Preloader.css';
 
 const Preloader: React.FC = () => {
     const [text, setText] = useState('');
-    const loadingMessages = [
-    'Welcome Geeks !',
-    ];
-
+    const loadingMessages = ['Welcome Geeks!']; // Your messages
     const [messageIndex, setMessageIndex] = useState(0);
 
     useEffect(() => {
         let typingIndex = 0;
+        let isTyping = true; // Track typing state
 
         const typeEffect = () => {
-            if (typingIndex < loadingMessages[messageIndex].length) {
-                setText((prev) => prev + loadingMessages[messageIndex][typingIndex]);
-                typingIndex++;
-            } else {
-                setTimeout(() => {
-                    setText('');
-                    typingIndex = 0;
-                    setMessageIndex((prev) => (prev + 1) % loadingMessages.length);
-                }, 1000); // Pause before switching to the next message
+            if (isTyping) {
+                if (typingIndex < loadingMessages[messageIndex].length) {
+                    setText((prev) => prev + loadingMessages[messageIndex][typingIndex]);
+                    typingIndex++;
+                } else {
+                    isTyping = false;
+                    setTimeout(() => {
+                        isTyping = true;
+                        setText('');
+                        typingIndex = 0;
+
+                        // Only increment the message index if there is more than one message
+                        if (loadingMessages.length > 1) {
+                            setMessageIndex((prev) => (prev + 1) % loadingMessages.length);
+                        }
+                    }, 1000); // Pause before resetting text
+                }
             }
         };
 
         const typingInterval = setInterval(typeEffect, 100);
 
         return () => clearInterval(typingInterval);
-    }, [messageIndex]);
+    }, [messageIndex, loadingMessages]);
 
     return (
         <div className="preloader">
